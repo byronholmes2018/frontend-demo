998cf1f78ca8d0964090d0a1a778098a083ee3a8
ByronHolmes-at-129020800614 <lshopeful2018@gmail.com>
2021-09-07 20:54:16 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    message: '',
+  };
+  componentDidMount() {
+    fetch('/demo')
+      .then((response) => response.json())
+      .then((data) => this.setState({ message: data.data }))
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
